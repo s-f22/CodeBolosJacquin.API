f2a8c6bd446f628799ada1b2b3756f605fd4216a
Samuel <samuel.silveira@sp.senai.br>
2026-06-02 13:10:57 -0300

    Aula 27/05/2026: implementação de UploadController

---
+++ CodeBolosJacquin.API/Controllers/UploadController.cs
@@ -0,0 +1,51 @@
+﻿using CodeBolosJacquin.API.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeBolosJacquin.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _enviroment;
+
+        public UploadController(IWebHostEnvironment enviroment)
+        {
+            _enviroment = enviroment;
+        }
+
+
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Upload([FromForm] UploadRequestViewModel request)
+        {
+            var file = request.File;
+
+            if (file == null || file.Length == 0)
+                return BadRequest("Arquivo não informado");
+
+            var uploadsFolder = Path.Combine(_enviroment.ContentRootPath, "Uploads");
+
+            Directory.CreateDirectory(uploadsFolder);
+
+            var fileName = Path.GetFileName(file.FileName);
+
+            var filePath = Path.Combine(uploadsFolder, fileName);
+
+            await using var fileStream = System.IO.File.Create(filePath);
+
+            await file.CopyToAsync(fileStream);
+
+            var relativePath = $"Uploads/{fileName}";
+
+            return Ok(new
+            {
+                CaminhoImagem = relativePath
+            });
+
+        }
+
+    }
+}
---
+++ CodeBolosJacquin.API/ViewModels/UploadRequestViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace CodeBolosJacquin.API.ViewModels
+{
+    public class UploadRequestViewModel
+    {
+        public IFormFile File { get; set; } = default!;
+    }
+}
